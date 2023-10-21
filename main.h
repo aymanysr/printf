4b1165a850d9d91c1c629e37c5dc5b5986d472a1
Aimane <aimaneyousr@gmail.com>
2023-10-20 17:31:53 -0700

    added int printf_int and printf_dec

--- main.h
+++ main.h
@@ -24,7 +24,8 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 int printf_char(va_list val);
 int printf_string(va_list val);
-size_t strlen(const char *str);
+size_t strlen(const char* str);
 int printf_37(void);
-
+int printf_dec(va_list args);
+int printf_int(va_list args);
 #endif
